9a6093d0481680c529b4661522ae9312eb356308
Ghofranne labidi <ghofrana744@gmail.com>
2023-06-16 13:02:39 +0100

    i added some screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sofiacare/pages/doctor/dossiers/page_dossier_perso.dart';
+import 'package:sofiacare/pages/patient/search/page_search_screen.dart';
 
 
 void main() {
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         ),
       ),
       debugShowCheckedModeBanner: false,
-      home:DossierPerso(),
+      home:SearchScreen(),
     );
   }
 }
--- lib/pages/doctor/dossiers/page_dossier.dart
+++ lib/pages/doctor/dossiers/page_dossier.dart
@@ -8,16 +8,9 @@ class MedicalDocument extends StatefulWidget {
 }
 
 class _MedicalDocumentState extends State<MedicalDocument> {
-  TextEditingController patientNameController = TextEditingController();
-  TextEditingController diagnosisController = TextEditingController();
-  TextEditingController medicationController = TextEditingController();
-  TextEditingController dobController = TextEditingController();
-  TextEditingController genderController = TextEditingController();
-  TextEditingController phoneController = TextEditingController(text: '+216');
-  TextEditingController emailController = TextEditingController();
-  TextEditingController CnamController = TextEditingController();
-  TextEditingController symptomsController = TextEditingController();
-  TextEditingController descriptionController = TextEditingController();
+  TextEditingController lastNameController = TextEditingController();
+  TextEditingController firstNameController = TextEditingController();
+  // Rest of the code...
 
   void _createMedicalRecord() {
     // Logic to create the medical record goes here
@@ -55,7 +48,7 @@ class _MedicalDocumentState extends State<MedicalDocument> {
               ),
               SizedBox(height: 10),
               TextFormField(
-                controller: patientNameController,
+                controller: lastNameController,
                 decoration: InputDecoration(
                   labelText: 'Nom *',
                   prefixIcon: Icon(Icons.person),
@@ -64,145 +57,35 @@ class _MedicalDocumentState extends State<MedicalDocument> {
               ),
               SizedBox(height: 10),
               TextFormField(
-                controller: patientNameController,
+                controller: firstNameController,
                 decoration: InputDecoration(
                   labelText: 'Prénom*',
                   prefixIcon: Icon(Icons.person),
                   border: OutlineInputBorder(),
                 ),
               ),
-              SizedBox(height: 10),
-              TextFormField(
-                controller: dobController,
-                decoration: InputDecoration(
-                  labelText: 'Date de naissance',
-                  prefixIcon: Icon(Icons.calendar_today),
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: RadioListTile<String>(
-                      title: const Text('Femme'),
-                      value: 'femme',
-                      groupValue: genderController.text,
-                      onChanged: (String? value) {
-                        setState(() {
-                          genderController.text = value!;
-                        });
-                      },
-                    ),
-                  ),
-                  Expanded(
-                    child: RadioListTile<String>(
-                      title: const Text('Homme'),
-                      value: 'homme',
-                      groupValue: genderController.text,
-                      onChanged: (String? value) {
-                        setState(() {
-                          genderController.text = value!;
-                        });
-                      },
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 10),
-              TextFormField(
-                controller: phoneController,
-                keyboardType: TextInputType.phone,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.phone),
-                  labelText: 'Numéro du téléphone',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 10),
-              TextFormField(
-                controller: emailController,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.email),
-                  labelText: 'Email@gmail.com',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 10),
-              TextFormField(
-                controller: CnamController,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.numbers),
-                  labelText: 'Num CNAM',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 10),
-              Text(
-                'Informations supplémentaires',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 10),
-              TextFormField(
-                controller: diagnosisController,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.local_hospital),
-                  labelText: 'Diagnostiques',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 10),
-              TextFormField(
-                controller: medicationController,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.medical_services),
-                  labelText: 'Médicaments',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 10),
-              TextFormField(
-                controller: symptomsController,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.sentiment_satisfied),
-                  labelText: 'Symptômes',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 10),
-              TextFormField(
-                controller: descriptionController,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.description),
-                  labelText: 'Description',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 20),
+              // Rest of the code...
               ElevatedButton(
-  onPressed: () {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => DossierPerso(),
-      ),
-    );
-  },
-  child: Text('Créer une fiche perso'),
-  style: ButtonStyle(
-    backgroundColor: MaterialStateProperty.resolveWith<Color>(
-      (Set<MaterialState> states) {
-        if (states.contains(MaterialState.pressed)) {
-          return Color(0xFF013871); // Color when the button is pressed
-        }
-        return Colors.blue; // Default color of the button
-      },
-    ),
-  ),
-),
-
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => DossierPerso(),
+                    ),
+                  );
+                },
+                child: Text('Créer une fiche perso'),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                    (Set<MaterialState> states) {
+                      if (states.contains(MaterialState.pressed)) {
+                        return Color(0xFF013871); // Color when the button is pressed
+                      }
+                      return Colors.blue; // Default color of the button
+                    },
+                  ),
+                ),
+              ),
             ],
           ),
         ),
